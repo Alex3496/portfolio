17e9ee30c6cde18b57efb9ab6a7dfc2f53aa5bfc
Manuel Elizondo <elizondo.manuel.101c@gmail.com>
2025-12-01 18:27:40 -0800

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Alex3496.github.io',
     base: 'portfolio',
+    vite: {    plugins: [tailwindcss()],  },
 });
